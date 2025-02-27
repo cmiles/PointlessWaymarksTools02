6cb63b908c6b7b82a04b889ecac4ca040feaa9b0
charl <>
2025-02-27 16:41:33 +0000

    WpfCommon: Better Orientation/Rotation support in the SimpleMediaPlayer
    FossilOrigin-Name: c439a1373652ffa1cdf9a49f1dff8726c522925e02160f70ad295eb676770ac7

--- PointlessWaymarks.WpfCommon/SimpleMediaPlayer/SimpleMediaPlayerControl.xaml.cs
+++ PointlessWaymarks.WpfCommon/SimpleMediaPlayer/SimpleMediaPlayerControl.xaml.cs
@@ -1,98 +1,131 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 using System.Windows.Threading;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Iptc;
+using MetadataExtractor.Formats.QuickTime;
 
-namespace PointlessWaymarks.WpfCommon.SimpleMediaPlayer
+namespace PointlessWaymarks.WpfCommon.SimpleMediaPlayer;
+
+/// <summary>
+///     Interaction logic for SimpleMediaPlayerControl.xaml
+/// </summary>
+public partial class SimpleMediaPlayerControl
 {
-    /// <summary>
-    /// Interaction logic for SimpleMediaPlayerControl.xaml
-    /// </summary>
-    public partial class SimpleMediaPlayerControl
+    private SimpleMediaPlayerContext? _context;
+    private DispatcherTimer? _timerVideoTime;
+    private bool _userIsDraggingSlider;
+
+    public SimpleMediaPlayerControl()
     {
-        private DispatcherTimer? _timerVideoTime;
+        InitializeComponent();
+    }
 
-        private bool _userIsDraggingSlider;
-
-        private SimpleMediaPlayerContext? _context;
-        
-
-        public SimpleMediaPlayerControl()
+    // Create the timer and otherwise get ready.
+    private void ControlLoaded(object sender, RoutedEventArgs e)
+    {
+        _timerVideoTime = new DispatcherTimer
         {
-            InitializeComponent();
-        }
+            Interval = TimeSpan.FromSeconds(0.1)
+        };
+        _timerVideoTime.Tick += TimerTick;
 
-        // Create the timer and otherwise get ready.
-        private void ControlLoaded(object sender, RoutedEventArgs e)
+        VideoContentPlayer.Stop();
+    }
+
+    private void SimpleMediaPlayerControl_OnDataContextChanged(object? sender, DependencyPropertyChangedEventArgs e)
+    {
+        _context = e.NewValue as SimpleMediaPlayerContext;
+    }
+
+    private void TimerTick(object? sender, EventArgs e)
+    {
+        if (VideoContentPlayer.Source != null && VideoContentPlayer.NaturalDuration.HasTimeSpan &&
+            !_userIsDraggingSlider)
+            VideoBarPosition.Value = VideoContentPlayer.Position.TotalMilliseconds;
+    }
+
+    private void Video_MediaOpened(object? sender, RoutedEventArgs e)
+    {
+        var file = VideoContentPlayer.Source.AbsolutePath;
+
+        IEnumerable<Directory> directories = ImageMetadataReader.ReadMetadata(file);
+
+        string? orientation = null;
+
+        foreach (var directory in directories)
         {
-            _timerVideoTime = new DispatcherTimer
+            if (!string.IsNullOrWhiteSpace(orientation)) break;
+
+            if (directory is ExifDirectoryBase)
             {
-                Interval = TimeSpan.FromSeconds(0.1)
-            };
-            _timerVideoTime.Tick += TimerTick;
+                var possibleDescription = directory.GetDescription(ExifDirectoryBase.TagOrientation);
+                if (!string.IsNullOrWhiteSpace(possibleDescription)) orientation = possibleDescription;
+            }
 
-            VideoContentPlayer.Stop();
+            if (directory is IptcDirectory)
+            {
+                var possibleDescription = directory.GetDescription(IptcDirectory.TagImageOrientation);
+                if (!string.IsNullOrWhiteSpace(possibleDescription)) orientation = possibleDescription;
+            }
+
+            if (directory is QuickTimeTrackHeaderDirectory)
+            {
+                var possibleDescription = directory.GetDescription(QuickTimeTrackHeaderDirectory.TagRotation);
+                if (!string.IsNullOrWhiteSpace(possibleDescription)) orientation = possibleDescription;
+            }
         }
 
-        private void TimerTick(object? sender, EventArgs e)
-        {
-            if (VideoContentPlayer.Source != null && VideoContentPlayer.NaturalDuration.HasTimeSpan &&
-                !_userIsDraggingSlider)
-                VideoBarPosition.Value = VideoContentPlayer.Position.TotalMilliseconds;
-        }
+        if (int.TryParse(orientation, out var transformAngle))
+            VideoContentPlayer.LayoutTransform = new RotateTransform(transformAngle * -1);
 
-        private void Video_MediaOpened(object? sender, RoutedEventArgs e)
-        {
-            VideoBarPosition.Minimum = 0;
-            VideoBarPosition.Maximum = VideoContentPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
-            VideoBarPosition.Visibility = Visibility.Visible;
-            Debug.Assert(_timerVideoTime != null, nameof(_timerVideoTime) + " != null");
-            _timerVideoTime.Start();
-        }
+        VideoBarPosition.Minimum = 0;
+        VideoBarPosition.Maximum = VideoContentPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+        VideoBarPosition.Visibility = Visibility.Visible;
+        Debug.Assert(_timerVideoTime != null, nameof(_timerVideoTime) + " != null");
+        _timerVideoTime.Start();
+    }
 
-        private void VideoPause_Click(object? sender, RoutedEventArgs e)
-        {
-            VideoContentPlayer.Pause();
-        }
+    private void VideoPause_Click(object? sender, RoutedEventArgs e)
+    {
+        VideoContentPlayer.Pause();
+    }
 
-        private void VideoPlay_Click(object? sender, RoutedEventArgs e)
-        {
-            VideoContentPlayer.Play();
-        }
+    private void VideoPlay_Click(object? sender, RoutedEventArgs e)
+    {
+        VideoContentPlayer.Play();
+    }
 
-        private void VideoProgress_DragCompleted(object? sender, DragCompletedEventArgs e)
-        {
-            _userIsDraggingSlider = false;
-            var sliderValue = (int)VideoBarPosition.Value;
-            var ts = new TimeSpan(0, 0, 0, 0, sliderValue);
-            VideoContentPlayer.Position = ts;
-        }
+    private void VideoProgress_DragCompleted(object? sender, DragCompletedEventArgs e)
+    {
+        _userIsDraggingSlider = false;
+        var sliderValue = (int)VideoBarPosition.Value;
+        var ts = new TimeSpan(0, 0, 0, 0, sliderValue);
+        VideoContentPlayer.Position = ts;
+    }
 
-        private void VideoProgress_DragStarted(object? sender, DragStartedEventArgs e)
-        {
-            _userIsDraggingSlider = true;
-        }
+    private void VideoProgress_DragStarted(object? sender, DragStartedEventArgs e)
+    {
+        _userIsDraggingSlider = true;
+    }
 
-        private void VideoProgress_ValueChanged(object? sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            VideoProgressStatus.Text = TimeSpan.FromMilliseconds(VideoBarPosition.Value).ToString(@"hh\:mm\:ss");
-            if(_context != null) _context.VideoPositionInMilliseconds = VideoBarPosition.Value;
-        }
+    private void VideoProgress_ValueChanged(object? sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        VideoProgressStatus.Text = TimeSpan.FromMilliseconds(VideoBarPosition.Value).ToString(@"hh\:mm\:ss");
+        if (_context != null) _context.VideoPositionInMilliseconds = VideoBarPosition.Value;
+    }
 
-        private void VideoRestart_Click(object? sender, RoutedEventArgs e)
-        {
-            VideoContentPlayer.Stop();
-            VideoContentPlayer.Play();
-        }
+    private void VideoRestart_Click(object? sender, RoutedEventArgs e)
+    {
+        VideoContentPlayer.Stop();
+        VideoContentPlayer.Play();
+    }
 
-        private void VideoStop_Click(object? sender, RoutedEventArgs e)
-        {
-            VideoContentPlayer.Stop();
-        }
-
-        private void SimpleMediaPlayerControl_OnDataContextChanged(object? sender, DependencyPropertyChangedEventArgs e)
-        {
-            _context = e.NewValue as SimpleMediaPlayerContext;
-        }
+    private void VideoStop_Click(object? sender, RoutedEventArgs e)
+    {
+        VideoContentPlayer.Stop();
     }
 }
