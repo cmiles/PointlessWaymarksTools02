134e7a20a502fdf582510ee7a04882ee24955968
cmiles <>
2025-03-14 17:16:12 +0000

    Common: New supporting files for TaggedDebouncedDelayedTaskQueu
    FossilOrigin-Name: af2f0f97966010f1fab7a95500f1e1102fd1c23339fdc65d56826d1a5a4fbcb0

---
+++ PointlessWaymarks.CommonTools/LimitedConcurrentQueue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace PointlessWaymarks.CommonTools;
+
+public class LimitedConcurrentQueue<T>(int limit) : ConcurrentQueue<T>
+{
+    public readonly int Limit = limit;
+
+    public new void Enqueue(T element)
+    {
+        base.Enqueue(element);
+        if (Count > Limit) TryDequeue(out var discard);
+    }
+}
---
+++ PointlessWaymarks.CommonTools/TaggedDebouncedDelayedRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PointlessWaymarks.CommonTools;
+
+public record TaggedDebouncedDelayedRequest
+{
+    public DateTimeOffset CreatedOn { get; init; } = DateTimeOffset.Now;
+    public string Source { get; init; } = string.Empty;
+    public string Tag { get; init; } = string.Empty;
+    public Func<string, Task> TaskFunc { get; init; } = _ => Task.CompletedTask;
+}
