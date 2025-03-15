650f2353346a70b16b21f4ab453b9ba69ba6b0eb
cmiles <>
2025-03-15 01:13:03 +0000

    Avalonia: Add a process helper class - only targets windows for now
    FossilOrigin-Name: 8ba45775efcdbf7336617bd44ab001819d0e5a5b54ece72966c0996b0615201a

---
+++ PointlessWaymarks.AvaloniaToolkit/Utility/ProcessHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace PointlessWaymarks.AvaloniaToolkit.Utility;
+
+public static class ProcessHelpers
+{
+    public static async Task OpenExplorerWindowForDirectory(string directoryName)
+    {
+        //TODO: Platform support!
+        await UiThreadSwitcher.ResumeForegroundAsync();
+
+        var ps = new ProcessStartInfo("explorer.exe", $" \"{directoryName}\"")
+        {
+            UseShellExecute = true,
+            Verb = "open"
+        };
+
+        Process.Start(ps);
+    }
+
+    public static async Task OpenExplorerWindowForFile(string fileName)
+    {
+        //TODO: Platform support!
+        await UiThreadSwitcher.ResumeForegroundAsync();
+
+        var args = $"/e, /select, \"{fileName.Replace("/", "\\")}\"";
+
+        var ps = new ProcessStartInfo { FileName = "explorer", Arguments = args };
+        Process.Start(ps);
+    }
+
+    public static void OpenUrlInExternalBrowser(string url)
+    {
+        //TODO: Platform support!
+        if (string.IsNullOrWhiteSpace(url)) return;
+        if (url == "about:blank") return;
+
+        var ps = new ProcessStartInfo(url) { UseShellExecute = true, Verb = "open" };
+        Process.Start(ps);
+    }
+}
