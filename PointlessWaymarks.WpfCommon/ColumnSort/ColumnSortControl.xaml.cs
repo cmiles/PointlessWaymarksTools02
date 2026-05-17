2b9587016dde323973289ec45a70248190e2a520
cmiles <>
2026-05-17 14:22:18 +0000

    WpfCommon: Add a height binding for the buttons to help with old (CMS) vs modern Wpf theme (everything else).
    FossilOrigin-Name: 008d9e0ec87597681f46f83d4719fd00687986ddd6a2d702138ab7686baecba5

--- PointlessWaymarks.WpfCommon/ColumnSort/ColumnSortControl.xaml.cs
+++ PointlessWaymarks.WpfCommon/ColumnSort/ColumnSortControl.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace PointlessWaymarks.WpfCommon.ColumnSort;
+﻿using System.Windows;
+
+namespace PointlessWaymarks.WpfCommon.ColumnSort;
 
 /// <summary>
 ///     Interaction logic for ColumnSortControl.xaml
 /// </summary>
 public partial class ColumnSortControl
 {
+    public static readonly DependencyProperty ButtonHeightProperty = DependencyProperty.Register(
+        nameof(ButtonHeight), typeof(double), typeof(ColumnSortControl), new PropertyMetadata(24D));
+
     public ColumnSortControl()
     {
         InitializeComponent();
+    }
+
+    public double ButtonHeight
+    {
+        get => (double)GetValue(ButtonHeightProperty);
+        set => SetValue(ButtonHeightProperty, value);
     }
 }
