4d70ad24d7bf9ede6f4e2f2276fc17708dc9cf29
cmiles <>
2026-07-04 18:06:14 +0000

    CommonTools: Add another Utf 8 StringWriter variation.
    FossilOrigin-Name: e8f71ea56f67aa58dad316b4e730de1401414c576cc6f0c01b748564e07e8fb2

--- PointlessWaymarks.CommonTools/Utf8StringWriter.cs
+++ PointlessWaymarks.CommonTools/Utf8StringWriter.cs
@@ -7,3 +7,9 @@ public class Utf8StringWriter : StringWriter
     //https://stackoverflow.com/questions/955611/xmlwriter-to-write-to-a-string-instead-of-to-a-file/955698#955698
     public override Encoding Encoding => Encoding.UTF8;
 }
+
+public sealed class Utf8StringWriterNoBom : StringWriter
+{
+    // The 'false' parameter explicitly tells the encoding NOT to emit a BOM.
+    public override Encoding Encoding { get; } = new UTF8Encoding(false);
+}
