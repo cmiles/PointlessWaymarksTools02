5499af0a840a07ecc28f8d83a0c0a10053bead50
cmiles <>
2026-08-22 14:48:08 +0000

    Warnings Work
    FossilOrigin-Name: 7ca816223de329d76e3da4b8974c19705caab84496b63ea63b23b276302c2c22

--- PointlessWaymarks.LlamaAspects/BlockingCommandAttribute.cs
+++ PointlessWaymarks.LlamaAspects/BlockingCommandAttribute.cs
@@ -1,5 +1,8 @@
-﻿namespace PointlessWaymarks.LlamaAspects;
+﻿using JetBrains.Annotations;
 
+namespace PointlessWaymarks.LlamaAspects;
+
+[MeansImplicitUse]
 public class BlockingCommandAttribute : Attribute
 {
 }
